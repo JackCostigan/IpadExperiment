d2a372d3966f9384ff2f2554e27354f123267226
Jack Costigan <jack.costigan@teckro.com>
2024-01-04 14:29:47 +0000

    Initial Commit

---
+++ IpadExperiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IpadExperiment
+//
+//  Created by Jack.Costigan on 04/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
