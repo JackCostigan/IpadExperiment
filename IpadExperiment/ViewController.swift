14097d8f93253f4892b2300c3121c36f0e9a0947
Jack Costigan <jack.costigan@teckro.com>
2024-01-04 15:23:51 +0000

    First commit.

--- IpadExperiment/ViewController.swift
+++ IpadExperiment/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello there"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSubviews()
     }
 
-
+    func setupSubviews() {
+        view.addSubview(label)
+        view.backgroundColor = .white
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
 
